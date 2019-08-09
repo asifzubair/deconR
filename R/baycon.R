7050d7de7fac04c31d225565be7ba8ab7f0bf5f6
asifzubair <asif.zubair@stjude.org>
2019-08-09 18:43:40 -0500

    baycon refactor

--- R/baycon.R
+++ R/baycon.R
@@ -8,7 +8,7 @@
 #' @param fit.mvn a logical indicating whether we want to fit a MVN to the posterior
 #' @param stanWarningCheck level at which Stan warnings must be handled, one of \code{c('none', 'divergent', 'all')}
 #' @param ... arguments to be passed to \code{rstan::sampling} (e.g. \code{chains, iter, init, verbose, refresh})
-#' @return An object of type vector (list) with posterior mean estimates (and MVN mean estimates),
+#' @return An object of type list with posterior mean estimates (and MVN mean estimates),
 #' alongwith associated error variances
 
 baycon <- function(numGenes = nrow(p_bulkExpressionSimMat), numCellTypes = ncol(p_simSigMatTwo),
@@ -33,22 +33,20 @@ baycon <- function(numGenes = nrow(p_bulkExpressionSimMat), numCellTypes = ncol(
     } else {
       nmfOut <- rstan::sampling(stanmodels$indSigmat, data = standata, ...);
     }
-
     stanSumNmf <- as.data.frame(nmfOut)
 
     # store the purity point estimtes here (we're using the posterior mean. *Would mode be better??? Probably*)
     # QUESTION: do we want to restrict the number of iterations for which mean is computed below ?
     pEstimatesList[[i]] <- apply(stanSumNmf[,1:numCellTypes], 2, mean)
-
-    if (fit.mvn)
-      mvnErrorDistList[[i]] <- mclust::mvn("XXX", stanSumNmf[, 1:numCellTypes], warn = T)
-
     # also capture the error estimates with a univariate normal distribution.
     # TODO?: we need to remove the warmup samples before estimating error
     # EDIT: this might be unnecessary as as.data.frame takes care of this
     # docs: Coerce the draws (without warmup) to an array, matrix or data frame. See as.array.stanfit.
     # NOTE: this gives unbiased estimate of the covariance matrix
     var[[i]] <- var(stanSumNmf[, 1:numCellTypes])
+
+    if (fit.mvn)
+      mvnErrorDistList[[i]] <- mclust::mvn("XXX", stanSumNmf[, 1:numCellTypes], warn = T)
 
     # print(paste("******************************************************* Done With Sample: ", i))
   }
@@ -72,5 +70,4 @@ baycon <- function(numGenes = nrow(p_bulkExpressionSimMat), numCellTypes = ncol(
     ret_value <- list(stan = stanEsts, mvn = mvnEsts)
 
   return(ret_value)
-
   }

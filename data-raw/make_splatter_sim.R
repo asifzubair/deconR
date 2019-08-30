a2abba9bfabf8bcce7ac1168840900fefcac6f3e
asifzubair <asif.zubair@stjude.org>
2019-08-30 16:34:22 -0500

    splt sim

--- data-raw/make_splatter_sim.R
+++ data-raw/make_splatter_sim.R
@@ -1,8 +1,6 @@
 ## code to prepare `make_splatter_sim` dataset goes here
 
-
 ## convenience functions
-
 `%>%` <- dplyr::`%>%`
 addNAs <- function(group, out.props) rep(NA, nrow(group) - sum(out.props))
 addLabels0 <- function(group, out.props, out.sample.names) c(unlist(mapply(rep, times = out.props, x = out.sample.names)), addNAs(group, out.props))
@@ -12,7 +10,6 @@ addLabels0 <- function(group, out.props, out.sample.names) c(unlist(mapply(rep, 
 ## so that we can avoid edge cases
 ## TODO: setting the seed seems to be problematic
 ## TODO: need to find a way to make this reproducible
-
 numCellTypes <- 4
 numCells <- 500
 numGenes <- 2000
@@ -24,21 +21,25 @@ total.samples <- numBulkSamples + numSCsamples + offset
 seed = 912345
 set.seed(seed)
 
-props <- sapply(rep(numCellTypes, total.samples), sample.simplex)
-total.props <- round(rowSums(props)/total.samples, 2)
+splat_props <- sapply(rep(numCellTypes, total.samples), deconR:::sample.simplex)
+total.props <- round(rowMeans(splat_props), 2)
 total.props <- total.props/sum(total.props)
-sim2 <- splatter::splatSimulate(group.prob = total.props, method = "group", verbose = F,
-                                nGenes = numGenes, batchCells = numCells*total.samples, seed = seed)
-sim2 <- scater::normalize(sim2)
-scater::plotPCA(sim2, colour_by = "Group")
 
-colData.mat <- SingleCellExperiment::colData(sim2)
-counts.mat <- SingleCellExperiment::counts(sim2)
+## Note that if the simplex RV generation works correctly,
+## I should have equal proportions in total.props,
+## especially when I generate a large number of samples.
+splat_sim <- splatter::splatSimulate(group.prob = total.props, method = "group", verbose = F,
+                                nGenes = numGenes, batchCells = numCells*total.samples, seed = seed)
+splat_sim <- scater::normalize(splat_sim)
+scater::plotPCA(splat_sim, colour_by = "Group")
+
+colData.mat <- SingleCellExperiment::colData(splat_sim)
+counts.mat <- SingleCellExperiment::counts(splat_sim)
 
 ## we attach a label signifying to which sample (bulk or single cell) is a particular cell assigned
-## we use the colData(sim) DataFrame to keep track of the label
+## we use the colData(splat_sim) DataFrame to keep track of the label
 
-out.props <- round(props[,-((total.samples - offset + 1):total.samples)]*numCells)
+out.props <- round(splat_props[,-((total.samples - offset + 1):total.samples)]*numCells)
 out.sample.names <- c(paste0("Bulk", seq(numBulkSamples)), paste0("SC", seq(numSCsamples)))
 addLabels <- function(...) addLabels0(out.sample.names = out.sample.names, ...)
 
@@ -70,18 +71,16 @@ for (group in colnames(splat_sigMat)){
 }
 
 # TODO: I have some testing code here, that needs to be removed.
-
-t(props[,1:5])
+t(splat_props[,1:5])
 
 out <- baycon(bulkExpression = splat_bulkExpression, sigMat = splat_sigMat, useHyperPrior = T, refresh = 0, iter = 3000)
 out$stan$mean
 
 # let's also look at how linear regression does
-
 pEstimate = list()
 for (i in 1:5)
   pEstimate[[i]] <- coef(summary(lm(splat_bulkExpression[,i] ~ ., data = splat_sigMat)))[2:5,1]
 lmEsts <- do.call(rbind, pEstimate)
 lmEsts
 
-# usethis::use_data(splat_bulkExpression, splat_sigMat, overwrite = T)
+usethis::use_data(splat_sim, splat_props, splat_sigMat, splat_bulkExpression, overwrite = T)
